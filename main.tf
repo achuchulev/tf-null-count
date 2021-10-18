a8d324fcdd4ef3ca72301b2990b140039dbc167a
Atanas Chuchulev <atanas.v4@gmail.com>
2021-10-18 12:43:28 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 resource "null_resource" "MultiHelloWorld" {
   count = var.multiple
   provisioner "local-exec" {
-    command = "sleep ${count.index+18}"
+    #command = "sleep ${count.index+18}"
+    command = "echo id: ${var.aws_access_key_id} key: ${var.aws_secret_access_key} ID: $AWS_ACCESS_KEY_ID  KEY: $AWS_SECRET_ACCESS_KEY"
   }
 }

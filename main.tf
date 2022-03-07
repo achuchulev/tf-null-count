b54073cfd1f8b4b0d476e20bd8d58fb354372cbc
Atanas Chuchulev <atanas.v4@gmail.com>
2022-03-07 17:31:57 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ resource "null_resource" "MultiHelloWorld" {
   count = var.multiple
   provisioner "local-exec" {
     #command = "sleep ${count.index+18}"
-    command = "which python3"
+    command = "python3 --version"
   }
 }

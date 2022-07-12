869b5c25b8b48ccd6030a92417eebb82d58c5914
Atanas Chuchulev <atanas.v4@gmail.com>
2022-07-12 16:28:12 +0300

    Delete main.tf

--- modules/main.tf
+++
@@ -1,8 +0,0 @@
-module "random_name" {
-  source = "github.com/achuchulev/terraform-aws-ec2"
-  ami = "ami-0b9ecb12083282d75"
-  instance_type = "t3.micro"
-  subnet_id = "subnet-01299b7be81c72a58"
-  public_key = "atanas"
-  vpc_security_group_ids = ["sg-0c1650b4f6afad68b"]
-}

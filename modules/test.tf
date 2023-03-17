b0d1fac8fefeb8d4e647008faf5bdf177e86e428
Atanas Chuchulev <atanas.v4@gmail.com>
2023-03-17 12:45:08 +0200

    Update test.tf

--- modules/test.tf
+++ modules/test.tf
@@ -6,3 +6,12 @@ resource "null_resource" "MultiHelloWorld" {
     command = "echo ${TFC_WORKSPACE}"
   }
 }
+
+resource "null_resource" "MultiHelloWorld2" {
+  triggers = {
+    uuid = uuid()
+  }
+  provisioner "local-exec" {
+    command = "echo ${TFC_WORKSPACE}"
+  }
+}
